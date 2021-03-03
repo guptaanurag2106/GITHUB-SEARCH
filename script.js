156fe694a5d45ef77e9770e1f3f559e334c8ab02
ANURAG GUPTA <75490384+guptaanurag2106@users.noreply.github.com>
2021-03-03 23:45:43 +0530

    sorted the search results according to forks

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ function showData() {
     var org = document.getElementById("org").value;
     var n = document.getElementById("nrepo").value;
     var m = document.getElementById("mcom").value;
-    console.log(org)
-    console.log(n)
-    console.log(m)
+
     var repo_list = [];
+    var repo_name = [];
+    var repo_url = [];
     var para1 = document.getElementById("data")
 
     const headers = {
         "Authorization": 'Token c6777076190c33e4d1bee37da656860e809a8846'
     }
 
-    
+    function compare(a, b) {
+        if (a.forks < b.forks) {
+            return 1;
+        }
+        if (a.forks > b.forks) {
+            return -1;
+        }
+        return 0;
+    }
+
+
     function def(data) {
         for (i = 0; i < data.length; i++) {
             repo_list.push(data[i]);
+            repo_list.sort(compare);
         }
     }
     for (var j = 1; j < 70; j++) {
@@ -25,6 +36,7 @@ function showData() {
         }).then(res => res.json()).then(data => def(data))
 
     }
+
     console.log(repo_list)
 
 }
