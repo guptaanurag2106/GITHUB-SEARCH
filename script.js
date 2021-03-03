60d7761517de3a9ebcddc238bdd2eeb1c0f9a93c
ANURAG GUPTA <75490384+guptaanurag2106@users.noreply.github.com>
2021-03-03 22:52:23 +0530

    added fetch for all data of an org

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-function showData(){
-    var org=document.getElementById("org").value;
-    var n=document.getElementById("nrepo").value;
-    var m=document.getElementById("mcom").value;
+function showData() {
+    var org = document.getElementById("org").value;
+    var n = document.getElementById("nrepo").value;
+    var m = document.getElementById("mcom").value;
     console.log(org)
     console.log(n)
     console.log(m)
-    var fork_count=[];
+    var repo_list = [];
+    var para1 = document.getElementById("data")
 
-    var para1=document.getElementById("data")
-    function def(data){
-    for (i=0;i<n;i++)
-        fork_count.push(data[i].forks)
-    para1.innerHTML=data[0].full_name+"<br>"+fork_count
-    
-    console.log(fork_count)
-    console.log(data)
+    const headers = {
+        "Authorization": 'Token c6777076190c33e4d1bee37da656860e809a8846'
     }
-    var url="https://api.github.com/orgs/"+org+"/repos"
-    fetch(url).then(res => res.json()).then(data => def(data))
+
     
+    function def(data) {
+        for (i = 0; i < data.length; i++) {
+            repo_list.push(data[i]);
+        }
+    }
+    for (var j = 1; j < 70; j++) {
+        var url = "https://api.github.com/orgs/" + org + "/repos?page=" + j.toString()
+        fetch(url, {
+            "method": "GET", "headers": headers
+        }).then(res => res.json()).then(data => def(data))
+
+    }
+    console.log(repo_list)
+
 }
